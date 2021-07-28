2e814a91aed904b439ab611474d89171fb4113ef
Safal Piya <mrsafalpiya@gmail.com>
2021-07-28 19:30:11 +0545

    Q15 solved

--- C assignment/15.hcflcm.c
+++ C assignment/15.hcflcm.c
@@ -1,1 +1,30 @@
-// idk kids math bruh
+#include <stdio.h>
+
+int main() {
+	int num1, num2, a, b, temp, hcf, lcm;
+
+	printf("Enter two numbers: ");
+	scanf("%d %d", &num1, &num2);
+
+	a = num1;
+	b = num2;
+
+	if (a < b) {
+		temp = a;
+		a = b;
+		b = temp;
+	}
+
+	while (b > 0) {
+		temp = b;
+		b = a % b;
+		a = temp;
+	}
+
+	hcf = a;
+	lcm = (num1 * num2) / hcf;
+	
+	printf("HCF: %d, LCM: %d\n", hcf, lcm);
+
+	return 0;
+}
