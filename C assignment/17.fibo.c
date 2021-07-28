b7a8f5540ffb87dc102a4fe25ecadb633c5433f2
GenVenom <51529050+GenVenom@users.noreply.github.com>
2021-07-28 19:35:59 +0545

    Update 17.fibo.c
    Updated 17 with full code

--- C assignment/17.fibo.c
+++ C assignment/17.fibo.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
 int main(){
-    int n;
-    printf("Enter the nth term for the fibonanci series: ");
-    scanf("%d",&n);
-
-
-    return 0;
+    int i,number;
+    int first_num = 0;
+    int second_num =1;
+    int new_num;
+    printf("Enter the nth term for the series: ");
+    scanf("%d",&number);
+    printf("%d\n%d\n",first_num,second_num);
+    
+    for (i=0;i<number-2;i++){
+        new_num = first_num + second_num;
+        first_num = second_num;
+        second_num = new_num;
+        printf("%d\n",new_num);
+    }
 }
